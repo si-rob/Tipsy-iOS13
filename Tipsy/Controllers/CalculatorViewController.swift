9dd0dd8df600edb23c096ac546b1bf36bb906e1e
Simon Roberts <simon@sirob.dev>
2021-01-03 22:29:20 -0600

    calculate tip

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -16,16 +16,45 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-
+    var tip = 0.1
+    var split: Int = 2
+    var billTotal = 0.0
+    
     @IBAction func tipChanged(_ sender: UIButton) {
+
+        billTextField.endEditing(true)
+        
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        sender.isSelected = true
+        
+        let buttonTitle = sender.currentTitle!
+        let buttonTitleMinusPercentSign = String(buttonTitle.dropLast())
+        let buttonTitleAsNumber = Double(buttonTitleMinusPercentSign)!
+        
+        tip = buttonTitleAsNumber / 100
+        
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        split = Int(sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+
+        let bill = billTextField.text!
+        
+        if bill != "" {
+            billTotal = Double(bill)!
+            
+            let result = billTotal * (1 + tip) / Double(split)
+            
+            let resultTo2DecimalPlaces = String(format: "%.2f", result)
+            
+            print(resultTo2DecimalPlaces)
+        }
     }
     
 }
-
-
